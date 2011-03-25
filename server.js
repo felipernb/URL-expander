0c65251ddd5bf16b9d53e2e99b22b986905426d8
Felipe Ribeiro <felipernb@gmail.com>
2011-03-25 14:31:44 +0100

    adding version number

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.write("Hello world by @felipernb\n");
-  res.end("Running on " +process.version);
+  res.end("Running on nodejs-" +process.version);
 });
   
 server.listen(process.env.PORT || 8001);
