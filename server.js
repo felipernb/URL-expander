ebd0a5f9c851e47a953a7a30aeb7a057fddec04b
Felipe Ribeiro <felipernb@gmail.com>
2011-03-25 14:55:27 +0100

    using express

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-		    res.send("Hello World by @felipernb\nRunning on nodejs-" + process.version + " and express");
+		    res.send("Hello World by @felipernb<br/>Running on nodejs-" + process.version + " with express");
 
 			});
 
