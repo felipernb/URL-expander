88eaea9ba9061db022e0ebfe5e57c91386e49a3c
Felipe Ribeiro <felipernb@gmail.com>
2011-05-24 14:08:45 +0200

    breaking line

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-		    res.send("Hello World by @felipernb<br/>Running on nodejs-" + process.version + " with express");
+		    res.send("Hello World by @felipernb<br/>
+				Running on nodejs-" + process.version + " with express");
 
 			});
 
