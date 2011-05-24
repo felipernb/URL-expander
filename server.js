68c231624ca19a9a88ae220bec1015096bdc95ae
Felipe Ribeiro <felipernb@gmail.com>
2011-05-24 14:18:13 +0200

    testing

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-		    res.send("Hello World by @felipernb<br/>
-				Running on nodejs-" + process.version + " with express");
+		    res.send("Hello World by @felipernb<br/>Running on nodejs-" + process.version + " with express");
 
 			});
 
