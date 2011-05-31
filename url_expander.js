d26c01254eeafc154d8b3e02c512fd0c7eee3371
Felipe Ribeiro <felipernb@gmail.com>
2011-05-31 22:24:55 +0200

    Managing cache

--- url_expander.js
+++ url_expander.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var cache = {};
+var cache_size = 0;
 exports.expand = function(rawUrl, func) {
 	var defaultText = "Not found";
 	if (!rawUrl || !rawUrl.match(/^[a-zA-Z0-9.\/?=&:-]+$/)) {
@@ -25,7 +26,12 @@ exports.expand = function(rawUrl, func) {
 	var req = http.get(options, function (res) {
 		if (res.headers.location) {
 			func(res.headers.location);
+			if (cache_size >= 10000) {
+				cache = {};
+				cache_size = 0;
+			}
 			cache[rawUrl] = res.headers.location;
+			cache_size++;
 		} else {
 			func(defaultText);
 		}
